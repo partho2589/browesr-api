c750909e95569bcd99d4dafdc038a55bc8543bed
partho2589 <parthomalo615@.com>
2022-03-04 10:40:27 +0600

    added all fiels

---
+++ explor.js
@@ -0,0 +1,9 @@
+const name = 'Partho malo'
+const student = {
+    name: 'Partho malo', age:20, class:'12', job:'web developer'
+    
+}
+student.age = 30;
+
+console.log(student)
+// document.getElementById('name')
