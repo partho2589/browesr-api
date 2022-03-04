b7dc97e4ff86c3b897f673024aae189e88eb5134
partho2589 <parthomalo615@.com>
2022-03-04 11:10:54 +0600

    adde aleat, confirm, prompt function

--- aleat.js
+++ aleat.js
@@ -1,1 +1,24 @@
 console.log('added all files')
+
+const commingM = () => {
+    alert ('Mom comming!! Open the book.')
+}
+const goingP = () => {
+  const response=  confirm ('You go to picnick')
+  console.log(response)
+     if ( response === true){
+         alert( 'taka de amar ka se bkash kore')
+     }else{
+         console.log('dure gia mor')
+     }
+
+}
+
+const askName = () => {
+    const name = prompt (
+        'Whats your name'
+    )
+    if( name){
+        console.log(name)
+    }
+}
